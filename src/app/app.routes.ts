34a08ad594ae94d059c9789e734f45a797a54e3f
Eduard <eduard.varnavskiy@atos.net>
2025-05-14 12:39:13 +0200

    pequeña modificacion en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../shared/components/login/login.component';
-import { RegistrarseComponent } from '../shared/components/registrarse/registrarse.component';
-import { ResenaFormComponent } from '../shared/components/resena-form/resena-form.component';
-import { VehiculosComponent } from '../shared/components/vehiculos/vehiculos.component';
-import { ReservaFormComponent } from '../shared/components/reserva-form/reserva-form.component';
-import { TipoVehiculoComponent } from '../shared/components/tipo-vehiculo/tipo-vehiculo.component';
-import { MisReservasComponent } from '../shared/components/mis-reservas/mis-reservas.component';
-import { AdminReservasComponent } from '../shared/components/admin-reservas/admin-reservas.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { RegistrarseComponent } from './shared/components/registrarse/registrarse.component';
+import { ResenaFormComponent } from './shared/components/resena-form/resena-form.component';
+import { VehiculosComponent } from './shared/components/vehiculos/vehiculos.component';
+import { ReservaFormComponent } from './shared/components/reserva-form/reserva-form.component';
+import { TipoVehiculoComponent } from './shared/components/tipo-vehiculo/tipo-vehiculo.component';
+import { MisReservasComponent } from './shared/components/mis-reservas/mis-reservas.component';
+import { AdminReservasComponent } from './shared/components/admin-reservas/admin-reservas.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'vehiculos', pathMatch: 'full' },
@@ -18,6 +18,6 @@ export const routes: Routes = [
   { path: 'mis-reservas', component: MisReservasComponent },
   { path: 'admin/reservas', component: AdminReservasComponent },
   { path: 'resena-form', component: ResenaFormComponent}
-  
+
 
 ];
