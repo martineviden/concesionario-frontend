8dd1f85e4bd8b235b08ed7e1217f38e17492d2ff
YanetCamac <yanet.camacho.external@eviden.com>
2025-05-20 10:10:16 +0200

    Add files via upload

---
+++ src/app/shared/components/dato-home/dato-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dato-home',
+  imports: [],
+  templateUrl: './dato-home.component.html',
+  styleUrl: './dato-home.component.css'
+})
+export class DatoHomeComponent {
+
+}
