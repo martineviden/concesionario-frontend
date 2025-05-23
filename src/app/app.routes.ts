9571cd52c559be7d35575f9c593cbc90ae3ac66e
yanetC <yanet.camacho.external@eviden.com>
2025-05-23 09:06:00 +0200

    una vista repetida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,11 @@ import { ReservaFormComponent } from './shared/components/reserva-form/reserva-f
 import { TipoVehiculoComponent } from './shared/components/tipo-vehiculo/tipo-vehiculo.component';
 import { MisReservasComponent } from './shared/components/mis-reservas/mis-reservas.component';
 import { AdminReservasComponent } from './shared/components/admin-reservas/admin-reservas.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HomeComponent } from './vistas/home/home.component';
 import { CatalogoComponent } from './vistas/catalogo/catalogo.component';
 import { EditarPerfilComponent } from './shared/components/editar-perfil/editar-perfil.component';
 import { NosotrosComponent } from './vistas/nosotros/nosotros.component';
 import { ContactoComponent } from './vistas/contacto/contacto.component';
-import { PanelAdministradorComponent } from './vistas/panel-administrador/panel-administrador.component';
 import { EspecificacionesComponent } from './vistas/especificaciones/especificaciones.component';
 import { PerfilComponent } from './vistas/perfil/perfil.component';
 
@@ -33,7 +31,6 @@ export const routes: Routes = [
   { path: 'editar-perfil', component: EditarPerfilComponent},
   { path: 'nosotros', component: NosotrosComponent},
   { path: 'contacto', component: ContactoComponent},
-  { path: 'administrador', component: PanelAdministradorComponent},
   {path: 'especificaciones', component: EspecificacionesComponent},
   {path: 'perfil', component: PerfilComponent}
 
