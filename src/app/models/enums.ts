0c4dc6f9b6f570fbf78b4534b2ad2f29219b54cc
Eduard <eduard.varnavskiy@atos.net>
2025-05-23 15:59:14 +0200

    guardar enum

--- src/app/models/enums.ts
+++ src/app/models/enums.ts
@@ -44,6 +44,10 @@ export namespace TipoVehiculo {
     let keys = Object.keys(TipoVehiculo);
     return keys.slice(0,keys.length-1)
   }
+
+  export function tipo(tipo: any, ubicacion: any) {
+    throw new Error("Function not implemented.");
+  }
 }
 
 export namespace Combustible {
