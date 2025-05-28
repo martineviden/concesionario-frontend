1e2e079ea431fd756ba240334f28c0ecfc914c52
martineviden <martin.guijarro.external@eviden.com>
2025-05-28 09:43:09 +0200

    Added comprobation for admin role

--- src/app/shared/components/tipo-vehiculo-banner-first/tipo-vehiculo-banner-first.component.ts
+++ src/app/shared/components/tipo-vehiculo-banner-first/tipo-vehiculo-banner-first.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormularioAlquilarComponent } from "../formulario-alquilar/formulario-alquilar.component";
+import { AuthService } from '../../../services/auth.service';
+import { Rol } from '../../../models/enums';
 
 @Component({
   selector: 'app-tipo-vehiculo-banner-first',
@@ -11,4 +13,17 @@ import { FormularioAlquilarComponent } from "../formulario-alquilar/formulario-a
 })
 export class TipoVehiculoBannerFirstComponent {
   mostrarFormularioAlquilar = false;
+  esAdmin: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  eliminarVehiculo() {
+    console.log("El vehiculo se tiene que eliminar");
+  }
+
+  ngOnInit() {
+    this.authService.obtenerUsuarioActual().subscribe(usuario => {
+      this.esAdmin = usuario?.rol === Rol.ADMIN;
+    });
+  }
 }
