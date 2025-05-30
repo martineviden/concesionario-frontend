e6b5646759cdeda2599f414bdf681799f25ba7c7
martineviden <martin.guijarro.external@eviden.com>
2025-05-30 14:49:29 +0200

    Deleted unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/login/login.component';
 import { RegistroComponent } from './shared/components/registro/registro.component';
-import { ResenaFormComponent } from './shared/components/resena-form/resena-form.component';
-import { VehiculosComponent } from './shared/components/vehiculos/vehiculos.component';
-import { ReservaFormComponent } from './shared/components/reserva-form/reserva-form.component';
-import { TipoVehiculoComponent } from './shared/components/tipo-vehiculo/tipo-vehiculo.component';
-import { MisReservasComponent } from './shared/components/mis-reservas/mis-reservas.component';
-import { AdminReservasComponent } from './shared/components/admin-reservas/admin-reservas.component';
 import { HomeComponent } from './vistas/home/home.component';
 import { CatalogoComponent } from './vistas/catalogo/catalogo.component';
 import { EditarPerfilComponent } from './shared/components/editar-perfil/editar-perfil.component';
@@ -20,12 +14,6 @@ export const routes: Routes = [
   // // { path: '', redirectTo: 'vehiculos', pathMatch: 'full' }, para que no me redireccione
   { path: 'login', component: LoginComponent },
   { path: 'registrarse', component: RegistroComponent },
-  { path: 'vehiculos', component: VehiculosComponent },
-  { path: 'tipo-vehiculo/:id', component: TipoVehiculoComponent },
-  { path: 'reservar/:matricula', component: ReservaFormComponent },
-  { path: 'mis-reservas', component: MisReservasComponent },
-  { path: 'admin/reservas', component: AdminReservasComponent },
-  { path: 'resena-form', component: ResenaFormComponent},
   { path: '', component: HomeComponent},
   { path: 'catalogo', component: CatalogoComponent},
   { path: 'editar-perfil', component: EditarPerfilComponent},
